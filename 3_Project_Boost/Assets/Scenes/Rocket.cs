f1654abe7439727a31a0d7ea138205b02697aa85
Harishkumarharry <54526806+Harishkumarharry@users.noreply.github.com>
2020-12-06 20:57:21 +0530

    Rocket Movement
    Added a rocket c# component and changed game object Rocket to Rocket Ship. Made Rocket movable if press space then it will go up.

---
+++ 3_Project_Boost/Assets/Scenes/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        processInput();
+    }
+
+    private void processInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Left key pressed");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Right key pressed");
+        }
+    }
+}
