ef900eea6b4cd4caae5772b82d61bb5baa118899
Harishkumarharry <54526806+Harishkumarharry@users.noreply.github.com>
2020-12-13 18:51:50 +0530

    Adding Audio
    Adding rocket rotational movement and adding audio to rocket when space bar is pressed.

--- 3_Project_Boost/Assets/Scenes/Rocket.cs
+++ 3_Project_Boost/Assets/Scenes/Rocket.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
-
+    AudioSource audiosource;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audiosource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,14 +25,23 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audiosource.isPlaying) //if we will not keep then the audio will keep on playing.
+            {
+                audiosource.Play();
+            }
+        }
+        else
+        {
+            audiosource.Stop();
         }
         if (Input.GetKey(KeyCode.A))
         {
-            print("Left key pressed");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("Right key pressed");
+            transform.Rotate(-Vector3.forward);
         }
+        //audiosource.Stop();
     }
 }
