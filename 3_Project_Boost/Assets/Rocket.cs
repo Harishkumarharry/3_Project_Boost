2c011aa6c12112b9ffa8de407dfb2184e8e3db6b
Harishkumarharry <54526806+Harishkumarharry@users.noreply.github.com>
2021-01-10 20:06:07 +0530

    Project Boost Complete V1
    Completing project boost as the course project completes.

---
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -0,0 +1,149 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[DisallowMultipleComponent]
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float rcsThrust = 250f;
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float levelLoadDelay = 1f;
+
+    [SerializeField] AudioClip RocketThrust;
+    [SerializeField] AudioClip Success;
+    [SerializeField] AudioClip Death;
+
+    [SerializeField] ParticleSystem RocketThrustParticle;
+    [SerializeField] ParticleSystem SuccessParticle;
+    [SerializeField] ParticleSystem DeathParticle;
+
+    enum State { Alive, Dying, Transcending};
+    State state = State.Alive;
+
+    bool CollisionDisabled = true;
+
+    Rigidbody rigidBody; //Getting components added in unity to handle in code
+    AudioSource audiosource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audiosource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(state == State.Alive)
+        {
+            RespondToThrustInput();//To Move the Rocket in upward direction
+            RespondToRotateInput();//To rotate the ship in either cloclwise
+        }
+        if (Debug.isDebugBuild)
+        {
+            RespondToDebugKey();
+        }
+    }
+
+    private void RespondToDebugKey()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadingNextLevel();
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            CollisionDisabled = !CollisionDisabled;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive || !CollisionDisabled) { return; } // Ignores collision when state is dead
+
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            default:
+                StartDeathSequence();
+                break;
+        }
+    }
+
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        audiosource.Stop();
+        audiosource.PlayOneShot(Success);
+        SuccessParticle.Play();
+        Invoke("LoadingNextLevel", levelLoadDelay);
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audiosource.Stop();
+        audiosource.PlayOneShot(Death);
+        DeathParticle.Play();
+        Invoke("LoadingFirstLevel", levelLoadDelay);
+    }
+
+    private void LoadingNextLevel()
+    {
+        int currentLevelIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextLevelIndex = currentLevelIndex + 1;
+        if(nextLevelIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextLevelIndex = 0;
+        }
+        SceneManager.LoadScene(nextLevelIndex);
+    }
+
+    private void LoadingFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void RespondToRotateInput()
+    {
+        rigidBody.angularVelocity = Vector3.zero;
+        
+        float rotationTrustperFrame = rcsThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationTrustperFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationTrustperFrame);
+        }
+    }    
+
+    private void RespondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Thrust();
+        }
+        else
+        {
+            audiosource.Stop();
+            RocketThrustParticle.Stop();
+        }
+    }
+
+    private void Thrust()
+    {
+        float mainThrustperFrame = mainThrust * Time.deltaTime;
+        rigidBody.AddRelativeForce(Vector3.up * mainThrustperFrame);
+        if (!audiosource.isPlaying) //if we will not keep then the audio will keep on playing.
+        {
+            audiosource.PlayOneShot(RocketThrust);
+        }
+        RocketThrustParticle.Play();
+    }
+}
